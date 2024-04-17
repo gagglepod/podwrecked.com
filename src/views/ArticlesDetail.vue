d2f8bd46af2f5a947409eff32facce9b47e36834
gagglepod <gagglepod@gmail.com>
2024-04-17 16:29:16 -0500

    Updated Articles and working out layout

--- src/views/ArticlesDetail.vue
+++ src/views/ArticlesDetail.vue
@@ -11,7 +11,7 @@
       v-if="post && post.img"
       :style="{
         backgroundImage:
-          'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\'/images/' +
+          'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url(\'/images/' +
           post.img +
           '\')',
       }"
@@ -19,13 +19,13 @@
       <div class="container">
         <header class="article-details-block__header">
           <p class="article-details__tags">
-            <span v-for="tag in post.tags" :key="tag" class="blog-tag-pill">{{
+            <span v-for="tag in post.tags" :key="tag" class="article-details-tag-pill">{{
               tag
             }}</span>
           </p>
           <h1 class="article-details__heading">{{ post.title }}</h1>
-          <p class="article-details__shortline">{{ post.subtitle }}</p>
-          <p class="article-details__author">By {{ post.author }}</p>
+          <h3 class="article-details__shortline">{{ post.summary }}</h3>
+          <p class="article-details__author">By {{ post.author }} | {{ post.date }}</p>
           <button class="btn btn--secondary btn--stretched" @click="router.back()">
             Back to Article List
           </button>
@@ -35,13 +35,13 @@
 
     <CloudRow />
 
-    <section class="articles-details-blob-block">
-      <div class="articles-details-container">
+    <section class="article-details-blob-block">
+      <div class="article-details-container">
         <!-- Adjusted container -->
         <!-- blog-post #1 -->
-        <article class="articles-details-blob">
-          <div class="articles-details-blob__content">
-            <div class="articles-details-blob__body" v-html="post.body"></div>
+        <article class="article-details-blob">
+          <div class="article-details-blob__content">
+            <div class="article-details-blob__body" v-html="post.body"></div>
           </div>
         </article>
       </div>
@@ -121,12 +121,12 @@ export default {
   background: var(--color-primary-alt);
   color: var(--color-headings);
   background-repeat: no-repeat;
-  background-position: center center; /* Adjusted background positioning */
+  background-position: center; /* Adjusted background positioning */
   background-size: cover;
   text-align: center;
 }
 
-.articles-details-error {
+.article-details-error {
   background: var(--color-dark);
   display: flex;
   flex-direction: row;
@@ -135,7 +135,7 @@ export default {
   margin-bottom: 0.5rem;
 }
 
-.articles-details-error-message {
+.article-details-error-message {
   font-size: 1.5rem;
   margin: 1rem;
   font-weight: bold;
@@ -145,7 +145,7 @@ export default {
   margin: 1rem 0;
 }
 
-.articles-details-tag-pill {
+.article-details-tag-pill {
   display: inline-block;
   margin: 10px 10px 0 0;
   color: var(--color-footer);
@@ -153,19 +153,20 @@ export default {
   padding: 8px;
   border-radius: 20px;
   font-size: 14px;
+  min-width: 50px;
 }
 
-.articles-details-tag-pill:hover {
+.article-details-tag-pill:hover {
   color: var(--color-placeholder);
   background: var(--color-footer);
 }
 
 .article-details-block__header {
   text-align: center;
-  margin: 2rem 0 2rem;
+  margin: 0; /* Remove margin */
 }
 
-.articles-details-container {
+.article-details-container {
   /* Adjusted container */
   max-width: 720px;
   margin: 0 auto;
@@ -176,10 +177,12 @@ export default {
 }
 
 .article-details__shortline {
-  font-size: 2rem;
+  font-size: 3rem;
   color: var(--color-headings);
   letter-spacing: 1px;
-  margin-top: 1rem 0 1rem;
+  line-height: 5rem;
+  margin: 1rem 0 1rem 0;
+  word-wrap: break-word; /* Ensure long words wrap */
 }
 
 .article-details__author {
@@ -195,37 +198,49 @@ p.article-details__content {
   margin: 1.6rem 0;
 }
 
-@media screen and (min-width: 768px) {
-  .article-details-block {
-    padding-top: 0;
+@media screen and (max-width: 768px) {
+  .block--article-details {
+    background-position: top; /* Adjusted background positioning */
   }
 
   .article-details__heading {
-    padding: 0 10rem 0 10rem;
+    font-size: calc(10vw + 1rem); /* Responsive font size adjustment */
+    line-height: 6rem; /* Adjusted line height */
   }
 
   .article-details__shortline {
-    padding: 2rem 10rem 0 10rem;
+    font-size: calc(2.2vw + 1rem); /* Responsive font size adjustment */
+    line-height: 2.5rem; /* Adjusted line height */
+    margin: 0 1rem; /* Adjust horizontal margin */
+  }
+
+  .article-details__author {
+    font-size: calc(1.5vw + 1rem); /* Responsive font size adjustment */
+    line-height: 3rem; /* Adjusted line height */
+  }
+
+  .article-details-blob__body {
+    margin: 0 1rem; /* Adjust horizontal margin */
   }
 }
 
-.articles-details-blob {
+.article-details-blob {
   padding: 2rem 0 2rem;
 }
 
-.articles-details-blob__body {
+.article-details-blob__body {
   font-size: 2.1rem;
   line-height: 3.2rem;
   margin: 0 2rem 0 2rem;
 }
 
-.articles-details-blob__body a {
+.article-details-blob__body a {
   color: var(--color-headings);
   transition: color 0.3s;
   text-decoration: underline;
 }
 
-.articles-details-blob__body a:hover {
+.article-details-blob__body a:hover {
   color: var(--color-placeholder);
   text-decoration: none;
 }
