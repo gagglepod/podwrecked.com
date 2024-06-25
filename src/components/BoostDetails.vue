7a695dba7738eb6061d2b4fafd9af4849c8a8c63
gagglepod <gagglepod@gmail.com>
2024-06-25 09:22:48 -0500

    Updated Boosts page

--- src/components/BoostDetails.vue
+++ src/components/BoostDetails.vue
@@ -4,7 +4,24 @@
       <!-- boost-detail #1 -->
       <article class="about-detail">
         <div class="about-detail__content">
-          <h2 class="about-detail__heading">Step 1</h2>
+          <h2 class="about-detail__heading">How to Boost</h2>
+          <p class="about-detail__info">
+            Boosting is how you show an artist how you value them. It is also a way for
+            the artist to benefit from their work while hearing directly from you!
+          </p>
+
+          <p class="about-detail__info">
+            However, before you can Boost you need to know <em>how</em> to Boost. The
+            following directions should help make you dangerous.
+            <strong>Happy Boosting!</strong>
+          </p>
+        </div>
+      </article>
+
+      <!-- boost-detail #2 -->
+      <article class="about-detail">
+        <div class="about-detail__content">
+          <h2 class="about-detail__heading">Step 1 - Get an App</h2>
           <p class="about-detail__info">
             First, you need a
             <a href="https://podcastindex.org/apps?appTypes=app" target="_blank"
@@ -18,32 +35,19 @@
         </div>
       </article>
 
-      <!-- boost-detail #2 -->
+      <!-- boost-detail #3 -->
       <article class="about-detail">
         <div class="about-detail__content">
-          <h2 class="about-detail__heading">Step 2</h2>
+          <h2 class="about-detail__heading">Step 2 - Find the Show</h2>
           <p class="about-detail__info">
             Second, you subscribe to
             <a href="https://podcastindex.org/podcast/6933361" target="_blank"
               >Flowgnar</a
             >
             on that app or you go over to
-            <a href="https://lnbeats.com" target="_blank">LN Beats</a> and see all the
-            other music podcasts out there There's about 20 of us to choose from right
+            <a href="https://lnbeats.com" target="_blank">LN Beats</a>, click on the <em>Music Shows</em> tab, and see all the
+            other music podcasts out there. There's about 22 of us to choose from right
             now!
-          </p>
-        </div>
-      </article>
-
-      <!-- boost-detail #3 -->
-      <article class="about-detail">
-        <div class="about-detail__content">
-          <h2 class="about-detail__heading">Step 3</h2>
-          <p class="about-detail__info">
-            Third, you use that modern podcast app to get yourself a
-            <a href="https://truefans.fm" target="_blank">wallet</a>. This is where you
-            turn your hard earn dollars into factions of a Bitcoin Called Satoshis -- or
-            SATS!
           </p>
         </div>
       </article>
@@ -51,14 +55,18 @@
       <!-- boost-detail #4 -->
       <article class="about-detail">
         <div class="about-detail__content">
-          <h2 class="about-detail__heading">Step 4</h2>
+          <h2 class="about-detail__heading">Step 3 - Fund Your Wallet</h2>
           <p class="about-detail__info">
-            Fourth, once you got a few SATS You go back and play this podcast -- or any
-            music podcast in your NEW, MODERN Podcast App. Then, when you hear me play a
-            song that you love you Boost them some SATs, which is to say you use that app
-            to send them some of your SATs, along with a short message directly to that
-            artist. And I mean directly! They see it. They read it. And they LOVE IT!
-            That's why it's called a BOOST!
+            Third, you need a wallet. A wallet is where you
+            turn your hard earn dollars into factions of a Bitcoin Called Satoshis -- or
+            SATs! Once you have a SATs, a modern podcast app will give you the capability to Boost any podcast that is set up to receive them.</p>
+            <p class="about-detail__info">
+            Understanding wallets can be tricky. Fortunately, many modern podcast apps like <a href="https://truefans.fm" target="_blank">Truefans</a> or <a href="https://podcastguru.io" target="_blank">Podcast Guru</a> can help you get a wallet within their apps.
+          </p>
+          <p class="about-detail__info">
+            However, if you want to own your own wallet you can use external wallet apps like <a href="https://getalby.com/" target="_blank">Get Alby</a>, <a href="https://breez.technology/" target="_blank">Breez</a>, or <a href="https://podcasterwallet.com/" target="_blank">Podcaster Wallet</a>. 
+            <p class="about-detail__info">
+            The goal is to get a wallet that will allow you to hold SATs. Once you have your own wallet, you can connect it to any modern podcast app. </p>
           </p>
         </div>
       </article>
@@ -66,11 +74,12 @@
       <!-- boost-detail #5 -->
       <article class="about-detail">
         <div class="about-detail__content">
-          <h2 class="about-detail__heading">Step 5</h2>
+          <h2 class="about-detail__heading">Step 4 - Send a Boost</h2>
           <p class="about-detail__info">
-            Finally, when you start boosting, make sure to tell them you heard it here on
-            Flowgnar! You can even Boost me too when I'm jabbering away between tracks!
-            Any time you feel like you got value you can give value back.
+            Fourth, once you got a few SATS in your wallet and that wallet is connected to your new, modern podcast app, it's time to Boost!</p>
+            <p class="about-detail__info">When you play Flowgnar on your modern podcast app you will see the option to boost the podcast or the artist while it is playing. If you like what you hear, Boost them some SATs, which is to say you use that app
+            to send them some of your SATs. That's it!</p>
+            <p class="about-detail__info">But wait, there's more! A boost can include a short messaage that goes directly to the artist or the podcaster too. And I mean directly! They see it. They read it. And they <em>LOVE IT!</em> That's why it's called a <em>BOOST!</em>
           </p>
         </div>
       </article>
@@ -78,15 +87,41 @@
       <!-- boost-detail #6 -->
       <article class="about-detail">
         <div class="about-detail__content">
+          <h2 class="about-detail__heading">Step 5 - Support the Show</h2>
+          <p class="about-detail__info">
+            Finally, when you start getting the hang of boosting, you will start boosting all the time -- you won't be able to stop! Just make sure when you boost a song you tell them you heard it here on Flowgnar! </p>
+            <p class="about-detail__info">
+            Boosting opens up a whole new world of communication, apperciation, and monitization between you, the listener, and the hosts and artist you hear on Flowgnar! If you like what you hear and want to hear more, don't forget that you can even boost me when I'm jabbering away between tracks!</p>
+            <p class="about-detail__info">
+            The best part of the Value 4 Value lifestyle is that any time you feel like you got value, you can give value back. Simple!
+          </p>
+        </div>
+      </article>
+
+      <!-- boost-detail #7 -->
+      <article class="about-detail">
+        <div class="about-detail__content">
           <h2 class="about-detail__heading">Bonus Step</h2>
           <p class="about-detail__info">
-            The goal is to support these artists or to quote the Podfather himself, Adam
-            Curry over at Boostagram Ball:
-            <em>"I play the tracks while the artist get the SATS!"</em> I do get a little
-            from each boost but that's for putting this all together.
+            The main goal of Flowgnar is to support the artists you hear on each episode. To quote the Podfather himself, Adam
+            Curry over at <a href="https://www.boostagramball.com/" target="_blank">Boostagram Ball (the show that started it all)</a>:
+            <em>"I play the tracks while the artist get the SATS!"</em>
           </p>
 
           <p class="about-detail__info">Now you know!</p>
+        </div>
+      </article>
+
+      <!-- boost-detail #8 -->
+      <article class="about-detail">
+        <div class="about-detail__content">
+          <h3 class="about-detail__heading"><em>*Fine Print*</em></h3>
+          <p class="about-detail__info">
+            <em>Flowgnar is a <a href="https://oncetold.us/" target="_blank">Oncetold Music Experiment</a> created to support <a href="https://value4value.info/" target="_blank">Valueverse</a> music
+            and was produced with the understanding that all songs are the property of
+            their respective artists. Flowgnar passes on 90 percent of all Boosts to the
+            artist while the song is playing.  I do get 10 percent from each boost to cover my part in putting Flowgnar together but a majority will always go to the artists.</em>
+          </p>
         </div>
       </article>
     </div>
